60d970f602bcd0b14225121e6b48a14887d5eb71
dwendisch <daniel.wendisch@gmail.com>
2022-12-02 11:03:39 +0100

    title of plot 2

--- plot2.R
+++ plot2.R
@@ -5,5 +5,5 @@ source_classification <- read_rds("Source_Classification_Code.rds") %>% as_tibbl
 
 
 
-p2 <- summary %>% filter(fips=="24510") %>% group_by(year) %>% summarise(tons_total=sum(tons)) %>% ggplot(aes(year,tons_total))+geom_col() 
-ggsave("plot2.png", plot = p2)+ggtitle("Baltimore")
+p2 <- summary %>% filter(fips=="24510") %>% group_by(year) %>% summarise(tons_total=sum(tons)) %>% ggplot(aes(year,tons_total))+geom_col() +ggtitle("Baltimore")
+ggsave("plot2.png", plot = p2)
